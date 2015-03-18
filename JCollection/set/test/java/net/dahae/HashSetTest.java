b3e1d2a8de47decb7f50dccd96d904998d4a53e2
다해정보기술 <net.dahae@gmail.com>
2015-03-18 10:37:22 +0900

    1

---
+++ JCollection/set/test/java/net/dahae/HashSetTest.java
@@ -0,0 +1,23 @@
+package net.dahae;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<Object> hs = new HashSet<Object>();
+		
+		hs.add(3);
+		hs.add(1);
+		hs.add(3);
+		hs.add("java");
+		hs.add("test");
+		hs.add("java");
+		hs.add(5);
+		
+		System.out.println(hs);
+
+	}
+
+}
